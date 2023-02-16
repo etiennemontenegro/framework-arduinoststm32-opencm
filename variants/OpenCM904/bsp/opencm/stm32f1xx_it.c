6374f0cdd373352b61099aaac9a8d84b5932275a
etiennemontenegro <43181133+etiennemontenegro@users.noreply.github.com>
2023-02-15 23:49:32 -0500

    test

--- variants/OpenCM904/bsp/opencm/stm32f1xx_it.c
+++ variants/OpenCM904/bsp/opencm/stm32f1xx_it.c
@@ -39,7 +39,7 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f1xx_it.h"
-#include "stm32f1xx_hal.h"
+#include "stm32f1xx_hal_msp.h"
 
 /** @addtogroup STM32F1xx_HAL_Examples
   * @{
